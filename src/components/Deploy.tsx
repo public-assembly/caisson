140586e8a3f7c61b0dd98927e54dd2a8d1558029
Salief Lewis <37086973+salieflewis@users.noreply.github.com>
2023-04-23 21:52:57 -0400

    Update Deploy.tsx

--- src/components/Deploy.tsx
+++ src/components/Deploy.tsx
@@ -2,7 +2,7 @@ export function Deploy() {
   return (
     <a
       className="hidden lg:block hover:text-black/50"
-      href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fsalieflewis%2Fcaisson&env=NEXT_PUBLIC_ALCHEMY_KEY"
+      href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fpublic-assembly%2Fcaisson&env=NEXT_PUBLIC_ALCHEMY_KEY"
       target="_blank"
       rel="noreferrer"
     >
