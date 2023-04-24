336f0fa62ed7c866537d2d2738fb1b3a7d2f5323
Salief <salieflewis@gmail.com>
2023-04-23 21:11:44 -0400

    add rome, remove eslint

--- src/components/Caisson.tsx
+++ src/components/Caisson.tsx
@@ -1,11 +1,11 @@
 export function Caisson() {
-  return (
-    <div className="flex gap-2">
-      <h1 className="font-semibold">Caisson</h1>
-      <p>
-        a watertight chamber used in construction work under water or as a
-        foundation
-      </p>
-    </div>
-  );
+	return (
+		<div className="flex gap-2">
+			<h1 className="font-semibold">Caisson</h1>
+			<p>
+				a watertight chamber used in construction work under water or as a
+				foundation
+			</p>
+		</div>
+	)
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
-import { ConnectKitButton } from "connectkit";
-import { Caisson } from "./Caisson";
+import { ConnectKitButton } from 'connectkit'
+import { Caisson } from './Caisson'
 
 export function Header() {
-  return (
-    <div className="flex justify-between items-center p-4">
-      <Caisson />
-      <ConnectKitButton />
-    </div>
-  );
+	return (
+		<div className="flex justify-between items-center p-4">
+			<Caisson />
+			<ConnectKitButton />
+		</div>
+	)
 }
