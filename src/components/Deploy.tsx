c5de7cd2f5d5c9b713428a57888c86276381a159
Salief <salieflewis@gmail.com>
2023-04-23 21:47:29 -0400

    add one click deploy

---
+++ src/components/Deploy.tsx
@@ -0,0 +1,12 @@
+export function Deploy() {
+  return (
+    <a
+      className="hidden lg:block hover:text-black/50"
+      href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fsalieflewis%2Fcaisson&env=NEXT_PUBLIC_ALCHEMY_KEY"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Deploy template
+    </a>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { Caisson } from './Caisson'
+export { Deploy } from './Deploy'
 export { Header } from './Header'
