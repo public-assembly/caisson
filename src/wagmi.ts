e70c8a09c17de96a285ac8790470ef9bba741672
Salief <salieflewis@gmail.com>
2023-06-13 13:57:26 -0400

    after rome

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,18 +1,18 @@
-'use client';
+'use client'
 
-import { getDefaultConfig } from 'connectkit';
-import { createConfig, configureChains } from 'wagmi';
-import { mainnet, optimism, arbitrum, goerli, sepolia } from 'wagmi/chains';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
+import { getDefaultConfig } from 'connectkit'
+import { createConfig, configureChains } from 'wagmi'
+import { mainnet, optimism, arbitrum, goerli, sepolia } from 'wagmi/chains'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
 
-const alchemyKey = process.env.NEXT_PUBLIC_ALCHEMY_KEY;
-const walletConnectID = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID;
+const alchemyKey = process.env.NEXT_PUBLIC_ALCHEMY_KEY
+const walletConnectID = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID
 
 const { chains } = configureChains(
   [mainnet, optimism, arbitrum, goerli, sepolia],
-  [alchemyProvider({ apiKey: alchemyKey as string }), publicProvider()]
-);
+  [alchemyProvider({ apiKey: alchemyKey as string }), publicProvider()],
+)
 
 export const config = createConfig(
   getDefaultConfig({
@@ -21,5 +21,5 @@ export const config = createConfig(
     appName: 'Caisson',
     autoConnect: true,
     chains,
-  })
-);
+  }),
+)
