566cd9a59b51f92ee37ceb8f0a351259fd28c5d5
Salief <salieflewis@gmail.com>
2023-08-25 18:34:49 -0400

    remove use client directive from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { Header } from '../components'
 
 export default function Page() {
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { ConnectKitButton } from 'connectkit'
 import { Caisson } from './Caisson'
 import { Deploy } from './Deploy'
